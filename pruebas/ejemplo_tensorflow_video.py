e78e968c9cc3238794a400f55b5f76013b836f2b
alvaroo-fdez <crackludensyt@gmail.com>
2024-01-25 13:39:42 +0100

    con el nuevo modelo

--- pruebas/ejemplo_tensorflow_video.py
+++ pruebas/ejemplo_tensorflow_video.py
@@ -11,7 +11,7 @@ def preprocesar_imagen(imagen):
     return imagen_aplanada[:14739].reshape((1, -1))  # Asegurar que tenga la forma correcta
 
 # Cargar el modelo previamente exportado
-model_path = "modelo\modelo_saved_model"
+model_path = "modelo_alvaro-1_convertido\modelo_saved_model"
 model = tf.keras.models.load_model(model_path)
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
@@ -27,13 +27,22 @@ alto_esperado = 100
 
 while True:
     # Capturar un cuadro de la cámara
+    
     ret, frame = cap.read()
+    
+    print("Valor de ret:", ret)
+    
+    frame = cv2.flip(frame, 1)
+
+    input = preprocesar_imagen(frame)
 
     # Realizar la inferencia con el modelo
-    predictions = model.predict(preprocesar_imagen(frame))
+    predictions = model.predict(input)
 
     # Obtener la clase predicha
     predicted_class = np.argmax(predictions)
+    
+    print("Predicciones del modelo:", predictions)
 
     # Mostrar el resultado en la pantalla
     cv2.putText(frame, f'Clase: {predicted_class}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
