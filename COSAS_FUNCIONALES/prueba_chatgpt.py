355e49e0fe51c5899ac28a96d4dca57e9b2de8e8
alvaroo-fdez <crackludensyt@gmail.com>
2024-02-05 14:21:51 +0100

    pruebas con openai

---
+++ COSAS_FUNCIONALES/prueba_chatgpt.py
@@ -0,0 +1,18 @@
+from openai import OpenAI
+API_KEY = "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"
+client = OpenAI(api_key=API_KEY)
+famoso = "Donald Trump"
+
+while True:
+    
+    prompt = input("\nIntroduce una pregunta: ")
+    
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": f"Eres {famoso}. Responde a las cuestiones como si fueras {famoso}"},
+            {"role": "user","content": prompt}
+        ]
+    )
+
+    print(completion.choices[0].message)
