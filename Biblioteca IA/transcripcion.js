c4dcc6a5f2f2d188316d0d92dd4b81cacd4158d3
alvaroo-fdez <crackludensyt@gmail.com>
2024-02-25 17:59:36 +0100

    transcripción añadida

--- Biblioteca IA/transcripcion.js
+++ Biblioteca IA/transcripcion.js
@@ -1,19 +1,33 @@
+// Verificamos si el navegador es compatible con la API de reconocimiento de voz y asignamos la implementación correspondiente
 var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-var recognition = new SpeechRecognition();
+console.log(SpeechRecognition);
+if (SpeechRecognition) {
+    // Instancia de SpeechRecognition
+    var recognition = new SpeechRecognition();
 
-recognition.onresult = function (event) {
+    // Input donde se monstrará la transcripción
     input = document.getElementById('user-message-input')
 
-    var current = event.resultIndex;
+    // Configuramos el evento onresult que se activa cuando se obtienen resultados del reconocimiento de voz
+    recognition.onresult = function (event) {
 
-    var transcript = event.results[current][0].transcript;
+        // Obtenemos el índice del resultado actual
+        var current = event.resultIndex;
 
-    input.value = transcript;
+        // Obtenemos la transcripción del resultado actual
+        var transcript = event.results[current][0].transcript;
+
+        // Asignamos la transcripción al valor del elemento de entrada de texto
+        input.value = transcript;
+    }
+
+    // Obtenemos la referencia al botón que activará el reconocimiento de voz
+    botonmic = document.getElementById('boton-mic');
+
+    // Configuramos el evento onclick del botón para iniciar el reconocimiento de voz
+    botonmic.onclick = function (e) {
+        // Iniciamos el reconocimiento de voz
+        recognition.start();
+    }
 }
 
-botonmic = document.getElementById('boton-mic');
-
-botonmic.onclick = function (event) {
-    recognition.start();
-    console.log('start');
-}
